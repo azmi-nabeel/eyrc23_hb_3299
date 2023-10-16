b606118eb0f2681b1d8e2549bb7eb004783696c1
Azmi Nabeel <azmihero158@gmail.com>
2023-10-16 16:00:58 +0530

    updated setup.py T1b

--- hb_task1b_ws/src/hb_task_1b/setup.py
+++ hb_task1b_ws/src/hb_task_1b/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+import glob
 
 package_name = 'hb_task_1b'
 
@@ -7,10 +9,16 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+    ('share/ament_index/resource_index/packages',
+    ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+    (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+    (os.path.join('share', package_name, 'config'), glob('config/*')),
+    (os.path.join('share', package_name, 'worlds'), glob('worlds/*'))
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='azmi',
